69edf64db18644e7e86610b0fe109a87527a09e5
Marina Lima <marinaarrudalima@gmail.com>
2024-02-25 20:34:05 -0300

    Exercise 2 - Rails Yelp MVP

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Restaurant.create!(name: 'Carlos', address: 'Rua Aspicuelta, 123', phone_number: '9999-1111', category: 'Italian')
+Restaurant.create!(name: 'Pizzaria Bras', address: 'Rua Padre Carvalho, 889', phone_number: '9999-2222', category: 'Italian')
+Restaurant.create!(name: 'Modern Mamma Osteria', address: 'Rua Mourato Coelho, 456', phone_number: '9999-3333', category: 'Italian')
+Restaurant.create!(name: 'A Casa do Porco', address: 'Rua Dias Ferreira, 467', phone_number: '9999-4444', category: 'French')
+Restaurant.create!(name: 'Bullguer', address: 'Rua Wisard, 34', phone_number: '9999-5555', category: 'Italian')
